f0ebe0b277e52307d2d9af615f40e55c7d1262d7
Esha1712 <krithikaesha@gmail.com>
2026-02-12 21:00:24 +0530

    implemented the theme logic

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import App from "./App";
+import { ThemeProvider } from "./context/ThemeContext";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
   </BrowserRouter>
 );
+
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,14 +1,19 @@
 import { useAuth } from "../hooks/useAuth";
 import { logout } from "../services/auth.service";
+import { useTheme } from "../context/ThemeContext";
 
 export default function ProfilePage() {
   const { user } = useAuth();
+  const { theme, toggleTheme } = useTheme();
 
   return (
     <div>
       <h2>Profile</h2>
       <p>Email: {user?.email}</p>
       <p>Role: {user?.role}</p>
+      <button onClick={toggleTheme}>
+        Switch to {theme === "light" ? "Dark" : "Light"} Mode
+      </button>
       <button onClick={logout}>Logout</button>
     </div>
   );
